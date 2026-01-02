7d585ab516deab58d3407190c80dd5867e83ba3f
alireza-haeri <alireza.haeri.dev@gmail.com>
2026-01-02 12:07:20 +0330

    inher tester

--- core/tester.sh
+++ core/tester.sh
@@ -1,13 +1,32 @@
 #!/usr/bin/env bash
 log "-i" "Starting test..."
-declare -A result
 
+# Tester function
+send_request() {
+  code=$(curl "${1}" -s -w "%{http_code}" -o /dev/null -L)
+  echo "$code" >> "$dataDirName/result"
+}
+
+# Run tester
 for ((i=1; i<="$2";i++))
   do
-    code=$(curl "${1}" -s -w "%{http_code}" -o /dev/null)
-    ((result["$code"]++))
+    send_request "$url" &
   done
   
+# Wait for requests
+wait
+
+cat "$dataDirName/result"
+  
+log "-w" "write"
+  
+# Write result
 . "$(dirname "${BASH_SOURCE[0]}")/writer.sh"
 
+# Remove temp file
+rm -f "$dataDirName/result"
+
 log "-i" "Ending test..."
+
+
+
