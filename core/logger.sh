4fad0bf360d2d91f2092b9dd5e1186dc90688b40
alireza-haeri <alireza.haeri.dev@gmail.com>
2026-01-02 11:45:21 +0330

    set text color for logs

--- core/logger.sh
+++ core/logger.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+RED='\e[31m'
+YELLOW='\e[33m'
+CYAN='\e[36m'
+NC='\e[0m'
+
 log(){
    case "$1" in
-      "-i") echo "[info] ${2}";;
-      "-w") echo "[warn] ${2}";;
-      "-e") echo "[err] ${2}";;
+      "-i") echo -e "${CYAN}[info] ${2}${NC}";;
+      "-w") echo -e "${YELLOW}[warn] ${2}${NC}";;
+      "-e") echo -e "${RED}[err] ${2}${NC}";;
       *) echo "log is: ${1}"
    esac
 }
